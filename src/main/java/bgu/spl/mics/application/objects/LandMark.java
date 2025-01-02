ef8bf538123e3815998556942862d837fdc7aae0
shiramanket <GitHub Email>
2025-01-02 19:14:49 +0200

    compilation errirs

--- src/main/java/bgu/spl/mics/application/objects/LandMark.java
+++ src/main/java/bgu/spl/mics/application/objects/LandMark.java
@@ -1,5 +1,5 @@
 package bgu.spl.mics.application.objects;
-
+import java.util.List;
 /**
  * Represents a landmark in the environment map.
  * Landmarks are identified and updated by the FusionSlam service.
