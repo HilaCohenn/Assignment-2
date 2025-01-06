7eb792e07b89e0f8ea1987ac84d0861d990cb57a
shiramanket <GitHub Email>
2025-01-06 18:26:46 +0200

    fhacn

--- src/main/java/bgu/spl/mics/application/objects/StampedCloudPoints.java
+++ src/main/java/bgu/spl/mics/application/objects/StampedCloudPoints.java
@@ -6,13 +6,13 @@ import java.util.List;
  * Used by the LiDAR system to store and process point cloud data for tracked objects.
  */
 public class StampedCloudPoints {
-    private final String id;
     private final int time;
+    private final String id;
     private final List<CloudPoint> cloudPoints; // in the json file its 2 doubles - need to create a cloud point
 
-    public StampedCloudPoints(String id, int time, List<CloudPoint> cloudPoints) {
-        this.id = id;
+    public StampedCloudPoints(int time,String id, List<CloudPoint> cloudPoints) {
         this.time = time;
+        this.id = id;
         this.cloudPoints = cloudPoints;
     }
 
